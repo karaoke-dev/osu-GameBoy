7120883bb554d376e9be8cd33821991ebf9587ea
為什麼 <andy840119@gmail.com>
2018-12-25 18:39:59 +0800

    喵

---
+++ osu.GameBoy.Tests.Nuget/AutomatedVisualTestGame.cs
@@ -0,0 +1,12 @@
+﻿using osu.Framework.Testing;
+
+namespace osu.Framework.Eggs.Gameboy.Tests
+{
+    internal class AutomatedVisualTestGame : TestGame
+    {
+        public AutomatedVisualTestGame()
+        {
+            Add(new TestBrowserTestRunner(new TestBrowser()));
+        }
+    }
+}
---
+++ osu.GameBoy.Tests.Nuget/TestGame.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using osu.Framework.Allocation;
+using osu.Framework.IO.Stores;
+
+namespace osu.Framework.Eggs.Gameboy.Tests
+{
+    internal class TestGame : Game
+    {
+        [BackgroundDependencyLoader]
+        private void load()
+        {
+            Resources.AddStore(new NamespacedResourceStore<byte[]>(new DllResourceStore(Assembly.GetExecutingAssembly().Location), "Resources"));
+        }
+    }
+}
