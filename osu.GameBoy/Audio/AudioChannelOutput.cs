5c1d1e82ff446303c47db227505f71656529c66e
andy840119 <andy840119@gmail.com>
2019-07-01 19:47:55 +0800

    先暫時改成這樣 感覺找到方向了 大概

--- osu.GameBoy/Audio/AudioChannelOutput.cs
+++ osu.GameBoy/Audio/AudioChannelOutput.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using Emux.GameBoy.Audio;
+using ManagedBass;
+using ManagedBass.Fx;
 using osu.Framework.Audio;
 using osu.Framework.Audio.Sample;
 using osu.Framework.Audio.Track;
@@ -11,41 +13,62 @@ namespace osu.Framework.Graphics.Eggs.Gameboy
 {
     public class AudioChannelOutput : Track ,IAudioChannelOutput
     {
+        private double currentTime;
+
+        private volatile bool isRunning;
+
+        /// <summary>
+        /// The handle for this track, if there is one.
+        /// </summary>
+        private int activeStream;
+
+        /// <summary>
+        /// The handle for adjusting tempo.
+        /// </summary>
+        private int tempoAdjustStream;
+
         public AudioChannelOutput(GameBoyMixer mixer, string name)
         {
             if (mixer == null)
                 throw new ArgumentNullException(nameof(mixer));
+
+            activeStream = Bass.CreateStream(SampleRate, 2, BassFlags.Decode | BassFlags.Prescan, StreamProcedureType.Push);
+
+            Bass.ChannelPlay(activeStream);
+
+            /*
+            // We assign the BassFlags.Decode streams to the device "bass_nodevice" to prevent them from getting
+            // cleaned up during a Bass.Free call. This is necessary for seamless switching between audio devices.
+            // Further, we provide the flag BassFlags.FxFreeSource such that freeing the activeStream also frees
+            // all parent decoding streams.
+            const int bass_nodevice = 0x20000;
+
+            //I don't know but just copy it
+            Bass.ChannelSetDevice(activeStream, bass_nodevice);
+            tempoAdjustStream = BassFx.TempoCreate(activeStream, BassFlags.Decode | BassFlags.FxFreeSource);
+            Bass.ChannelSetDevice(activeStream, bass_nodevice);
+            activeStream = BassFx.ReverseCreate(tempoAdjustStream, 5f, BassFlags.Default | BassFlags.FxFreeSource);
+
+
+            Bass.ChannelSetAttribute(activeStream, ChannelAttribute.TempoUseQuickAlgorithm, 1);
+            Bass.ChannelSetAttribute(activeStream, ChannelAttribute.TempoOverlapMilliseconds, 4);
+            Bass.ChannelSetAttribute(activeStream, ChannelAttribute.TempoSequenceMilliseconds, 30);
+            */
         }
-
-        public bool Enabled { get; set; } = true;
-
-        public int PlaybackConcurrency { get; set; }
-
-        public int SampleId { get; set; }
 
         public int SampleRate { get; } = 44100;
 
-        private double currentTime;
-
         public override double CurrentTime => currentTime;
-
-        private volatile bool isRunning;
 
         public override bool IsRunning => isRunning;
 
-        private SampleChannel sample;
-
         public void BufferSoundSamples(float[] sampleData, int offset, int length)
         {
-            //osu-framework的方法，不太適用
-            byte[] newSampleData = new byte[length * sizeof(float)];
-            if (Enabled)
-                Buffer.BlockCopy(sampleData, offset * sizeof(float), newSampleData, 0, length * sizeof(float));
+            Bass.StreamPutData(activeStream, sampleData, length);
 
-            sample = new Framework.Audio.SampleChannelBass(new SampleBass(newSampleData), (a) => { });
-            sample.Play();
+            
 
-            //用原本NAudio格式下去改
+            //audiobass's method
             /*
             byte[] newSampleData = new byte[length * sizeof(float)];
             if (Enabled)
